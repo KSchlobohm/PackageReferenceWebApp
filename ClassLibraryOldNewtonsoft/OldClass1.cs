97787fe74fed5bab2d39d43365b64a31744de459
Ken Schlobohm <keschlob@microsoft.com>
2021-03-29 15:30:53 -0500

    exploring package config

---
+++ ClassLibraryOldNewtonsoft/OldClass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryOldUnity
+{
+    public class OldClass1
+    {
+        public Unity.IUnityContainer GetContainer()
+        {
+            return new Unity.UnityContainer();
+        }
+    }
+}
---
+++ ClassLibraryRecentNewtonsoft/RecentClass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryRecentUnity
+{
+    public class RecentClass1
+    {
+        public Unity.IUnityContainer GetContainer()
+        {
+            return new Unity.UnityContainer();
+        }
+    }
+}
